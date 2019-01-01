a5c482af7ca42df0c23ac80aa97539792c8bae6f
lyrahgames <markuspawellek@gmail.com>
2019-01-01 19:33:41 +0100

    Create utilities header

--- exercises/05/activation.cc
+++ exercises/05/activation.cc
@@ -1,51 +1,16 @@
-#include <cmath>
-#include <functional>
-#include <iostream>
-
 #include <QApplication>
 
 #include "plot.h"
-
-template <typename Real>
-Real square(Real x) {
-  return x * x;
-}
-
-template <typename Real>
-struct sigmoid {
-  Real operator()(Real x) const noexcept {
-    return Real{1} / (Real{1} + std::exp(-x));
-  }
-  Real gradient(Real x) const noexcept {
-    const auto tmp = operator()(x);
-    return tmp * (Real{1} - tmp);
-  }
-};
-
-template <typename Real>
-struct sigtanh {
-  Real operator()(Real x) const noexcept {
-    return (Real{1} + std::tanh(x)) / Real{2};
-  }
-  Real gradient(Real x) const noexcept {
-    return (Real{1} - square(std::tanh(x))) / Real{2};
-  }
-};
-
-template <typename Function>
-auto nabla(Function&& f) {
-  return std::bind(&Function::gradient, std::forward<Function>(f),
-                   std::placeholders::_1);
-}
+#include "utils.h"
 
 int main(int argc, char* argv[]) {
   QApplication application(argc, argv);
 
-  Plot plot;
-  plot.plot_function("sigmoid", sigmoid<double>{}, -5, 5);
-  plot.plot_function("sigmoid'", nabla(sigmoid<double>{}), -5, 5);
-  plot.plot_function("sigtanh", sigtanh<double>{}, -5, 5);
-  plot.plot_function("sigtanh'", nabla(sigtanh<double>{}), -5, 5);
+  Plot plot{};
+  plot.plot_function("sigmoid", sigmoid<double>{}, -5, 5)
+      .plot_function("sigmoid'", nabla(sigmoid<double>{}), -5, 5)
+      .plot_function("sigtanh", sigtanh<double>{}, -5, 5)
+      .plot_function("sigtanh'", nabla(sigtanh<double>{}), -5, 5);
 
   return application.exec();
 }
---
+++ exercises/05/utils.h
@@ -0,0 +1,39 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
+#include <cmath>
+#include <functional>
+
+template <typename Real>
+constexpr Real square(Real x) noexcept {
+  return x * x;
+}
+
+template <typename Real>
+struct sigmoid {
+  constexpr Real operator()(Real x) const noexcept {
+    return Real{1} / (Real{1} + std::exp(-x));
+  }
+  constexpr Real gradient(Real x) const noexcept {
+    const auto tmp = operator()(x);
+    return tmp * (Real{1} - tmp);
+  }
+};
+
+template <typename Real>
+struct sigtanh {
+  constexpr Real operator()(Real x) const noexcept {
+    return (Real{1} + std::tanh(x)) / Real{2};
+  }
+  constexpr Real gradient(Real x) const noexcept {
+    return (Real{1} - square(std::tanh(x))) / Real{2};
+  }
+};
+
+template <typename Function>
+constexpr auto nabla(Function&& f) noexcept {
+  return std::bind(&Function::gradient, std::forward<Function>(f),
+                   std::placeholders::_1);
+}
+
+#endif  // UTILS_H_
